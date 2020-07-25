3dd37284f844821a7a8903d08d4ba5e7d27f179a
Glitch (hello-flask) <none>
2020-07-25 23:57:16 +0000

    🚞👋 Checkpoint ./public/client.js:15115715/29

--- public/client.js
+++ public/client.js
@@ -7,6 +7,7 @@ $(function() {
       console.log("button clicked!");
     
       $.get('/background_process_test', function(data) {
+        console.log(data);
         $('<p></p>').text(data).appendTo('#button-container');
       });
     
